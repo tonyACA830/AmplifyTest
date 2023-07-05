d1d63d5b92535655679e32dc9b663f48a7489673
tonyaca830 <tonya.c830@proton.me>
2023-07-05 05:37:56 -0400

    initial backend

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,15 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-  "auth": {
-    "amplifytest00bc4fd2": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "HostedUIDomain": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "OAuthMetadata": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
-  }
-}
+export type AmplifyDependentResourcesAttributes = {}
